e5ec1ed05e7de8d2d1fb9b2bf8cb86c71c5a0e45
Ciallo <dison2233@126.com>
2024-10-14 20:06:55 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -10,14 +10,15 @@ if __name__ == '__main__':
     game_path = r"E:\Steam\steamapps\common\Counter-Strike Global Offensive\game"
     file_path = game_path + r"/bin/win64/" + module + ".dll"
 
-    mem = PEMemory(r'E:\cs2bins\2024-10-8\engine2.dll')
-    vtable = mem.get_vtable_by_name("CEngineWatchdogThread")
-    helper = mem.rtti_helper.get_object_locator(vtable)
+    mem = PEMemory(file_path, init_type_inherits=True)
+    print(json.dumps(mem.type_inherits, indent=4))
+    #vtable = mem.get_vtable_by_name("TEST_H")
+    #helper = mem.rtti_helper.get_exact_inherits(vtable)
     # vlen = mem.get_vtable_length("CEngineWatchdogThread")
     # print(vlen)
 
-    mem.init_type_descriptor_names()
+    # mem.init_type_descriptor_names()
 
-    print(json.dumps(mem.type_descriptor_names, indent=4))
-    name = mem.undecorate_symbol_name(".?AV?$_Func_impl_no_alloc@V<lambda_1>@?1??AddViewsToSceneSystemForSplitScreenSlot@CRenderingWorldSession@@AEAAXAEBUSplitscreenViewParams_t@3@@Z@XPEAVISceneView@@AEBURenderViewport_t@@PEAUHSceneViewRenderTarget__@@PEAU7@@std@@")
-    print(name)
+
+    #name = mem.undecorate_symbol_name(".?AV?$_Func_impl_no_alloc@V<lambda_1>@?1??AddViewsToSceneSystemForSplitScreenSlot@CRenderingWorldSession@@AEAAXAEBUSplitscreenViewParams_t@3@@Z@XPEAVISceneView@@AEBURenderViewport_t@@PEAUHSceneViewRenderTarget__@@PEAU7@@std@@")
+    #print(name)
