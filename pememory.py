274915f5f3139e81467246984315871ae8779bf2
Ciallo <dison2233@126.com>
2024-10-14 18:17:51 +0800

    rtti helper

--- pememory.py
+++ pememory.py
@@ -1,22 +1,42 @@
 import pefile
 import capstone
+import ctypes
+from ctypes.wintypes import *
 
+
+def RaiseIfZero(result, func=None, arguments=()):
+    """
+    Error checking for most Win32 API calls.
+
+    The function is assumed to return an integer, which is C{0} on error.
+    In that case the C{WindowsError} exception is raised.
+    """
+    if not result:
+        raise ctypes.WinError()
+    return result
 
 class PEMemory:
     INVALID_ADDRESS = -1
 
-    def __init__(self, file_path: str):
+    def __init__(self, file_path: str, init_runtime_functions: bool = False):
+        self.dbghelp = ctypes.windll.dbghelp
         self.pe = pefile.PE(file_path, fast_load=True)
+        self.rtti_helper = RTTIHelper(self)
         self.sig_maker = SigMaker(self)
 
-        # str : list
-        # vtable_name -> [address]
-        self.vtable_cache = {}
+        self.runtime_data = self.get_section(".data")
+        self.readonly_data = self.get_section(".rdata")
 
-        # int : int
+        # vtable_name -> [address]
+        self.vtable_cache: dict[str, list] = {}
+
         # fn_start -> fn_end
-        self.runtime_functions = {}
-        self.init_runtime_functions()
+        self.runtime_functions: dict[int, int] = {}
+        if init_runtime_functions:
+            self.init_runtime_functions()
+
+        # type_descriptor_name -> vtable_addr
+        self.type_descriptor_names: dict[str, int] = {}
 
     @staticmethod
     def to_ida_pattern(byte_list) -> str:
@@ -58,6 +78,37 @@ class PEMemory:
                     return None
         return None
 
+    def get_int(self, addr: int):
+        res = int.from_bytes(self.read_address(addr, 4, False), byteorder='little')
+        return res if res != 0xFFFFFFFF else -1
+
+    def get_long(self, addr: int):
+        return int.from_bytes(self.read_address(addr, 8, False), byteorder='little')
+
+    def get_string(self, addr: int, section: pefile.SectionStructure = None):
+        if section is not None:
+            return self.get_string_by_section(addr, section)
+
+        for s in self.pe.sections:
+            res = self.get_string_by_section(addr, s)
+            if res != PEMemory.INVALID_ADDRESS:
+                return res
+        return PEMemory.INVALID_ADDRESS
+
+    def get_string_by_section(self, addr: int, section: pefile.SectionStructure):
+        section_start = section.VirtualAddress
+        section_end = section.VirtualAddress + section.Misc_VirtualSize
+        if section_start <= addr < section_end:
+            start_index = addr - section_start
+            section_data = section.get_data()
+            section_len = len(section_data)
+            if start_index < section_len:
+                end_index = start_index
+                while end_index < section_len and section_data[end_index] != 0:
+                    end_index += 1
+                return section_data[start_index:end_index].decode('utf-8')
+        return PEMemory.INVALID_ADDRESS
+
     def get_section(self, name: str):
         for section in self.pe.sections:
             section_name = section.Name.decode('utf-8').rstrip('\x00')
@@ -67,7 +118,7 @@ class PEMemory:
 
     def resolve_relative_address(self, addr: int, offset_register=0x3, offset_next_instruction=0x7) -> int:
         skip_register = addr + offset_register
-        relative_addr = int.from_bytes(self.read_address(skip_register, 4, False), byteorder='little')
+        relative_addr = self.get_int(skip_register)
         next_instruction = addr + offset_next_instruction
         return next_instruction + relative_addr
 
@@ -92,18 +143,10 @@ class PEMemory:
         if len(vtable_name) == 0:
             return PEMemory.INVALID_ADDRESS
 
-        runtime_data = self.get_section(".data")
-        if runtime_data is None:
-            return PEMemory.INVALID_ADDRESS
-
-        readonly_data = self.get_section(".rdata")
-        if readonly_data is None:
-            return PEMemory.INVALID_ADDRESS
-
         decorated_table_name = vtable_name if decorated \
             else ".?AV" + vtable_name + "@@"
 
-        type_descriptor_name = self.find_pattern_by_str(decorated_table_name, runtime_data)
+        type_descriptor_name = self.find_pattern_by_str(decorated_table_name, self.runtime_data)
         if type_descriptor_name is PEMemory.INVALID_ADDRESS:
             return PEMemory.INVALID_ADDRESS
 
@@ -112,14 +155,14 @@ class PEMemory:
 
         current_offset = 0
         while (current_offset := self.find_pattern_by_bytes(
-                rtti_type_descriptor_bytes, readonly_data, current_offset, False)) != PEMemory.INVALID_ADDRESS:
-            reference = self.get_address(current_offset, readonly_data)
+                rtti_type_descriptor_bytes, self.readonly_data, current_offset, False)) != PEMemory.INVALID_ADDRESS:
+            reference = self.get_address(current_offset, self.readonly_data)
             val1 = int(self.read_address(reference - 0xC, 1)[0], 16)
             val2 = int(self.read_address(reference - 0x8, 1)[0], 16)
             if val1 == 1 and val2 == 0:
                 offset_reference = reference - 0xC + self.pe.OPTIONAL_HEADER.ImageBase
                 offset_reference_bytes = offset_reference.to_bytes(8, byteorder='little')
-                rtti_complete_object_locator = self.find_pattern_by_bytes(offset_reference_bytes, readonly_data)
+                rtti_complete_object_locator = self.find_pattern_by_bytes(offset_reference_bytes, self.readonly_data)
                 if rtti_complete_object_locator != PEMemory.INVALID_ADDRESS:
                     return rtti_complete_object_locator + 0x8
             current_offset += 0x4
@@ -129,8 +172,7 @@ class PEMemory:
         if int(self.read_address(vtable_fn)[7], 16) != 0x00:
             return False
 
-        fn_start = int.from_bytes(self.read_address(vtable_fn, cast_list=False),
-                                  byteorder='little') - self.pe.OPTIONAL_HEADER.ImageBase
+        fn_start = self.get_long(vtable_fn) - self.pe.OPTIONAL_HEADER.ImageBase
         opcode = self.read_address(fn_start, 1)
         if opcode is None:
             return False
@@ -165,7 +207,7 @@ class PEMemory:
 
         fn = self.vtable_cache[vtable_name][target_offset]
         if use_dq_offset:
-            dq_offset = int.from_bytes(self.read_address(fn, cast_list=False), byteorder='little')
+            dq_offset = self.get_long(fn)
             return dq_offset - self.pe.OPTIONAL_HEADER.ImageBase
         return fn
 
@@ -178,12 +220,153 @@ class PEMemory:
         section_len = procedure_data.VirtualAddress + procedure_data.Misc_VirtualSize
 
         while current_offset < section_len:
-            start_addr = int.from_bytes(self.read_address(current_offset, 4, cast_list=False), byteorder='little')
-            end_addr = int.from_bytes(self.read_address(current_offset + 4, 4, cast_list=False), byteorder='little')
-            # unwind_info_address = int.from_bytes(self.read_address(current_offset + 8, 4, cast_list=False), byteorder='little')
+            start_addr = self.get_int(current_offset)
+            end_addr = self.get_int(current_offset + 4)
+            # unwind_info_address = self.get_int(current_offset + 8)
             self.runtime_functions[start_addr] = end_addr
             current_offset += 12
 
+    def undecorate_symbol_name(self, symbol_name):
+        symbol_name = symbol_name.replace('.?AV?', '?').replace('.?AV', '?')
+
+        flags = 0x1000 #UNDNAME_NAME_ONLY
+        _UnDecorateSymbolNameA = self.dbghelp.UnDecorateSymbolName
+        _UnDecorateSymbolNameA.argtypes = [LPSTR, LPSTR, DWORD, DWORD]
+        _UnDecorateSymbolNameA.restype = DWORD
+        _UnDecorateSymbolNameA.errcheck = RaiseIfZero
+
+        buffer = ctypes.create_string_buffer(512)
+        _UnDecorateSymbolNameA(symbol_name.encode('utf-8'), buffer, ctypes.sizeof(buffer), flags)
+        return buffer.value.decode('utf-8')
+
+    def init_type_descriptor_names(self):
+        filter_type_name = [
+            '<lambda',
+        ]
+        offset = 0
+        while (offset := self.find_pattern_by_str('.?AV', self.runtime_data, start_offset=offset, to_addr=False)) != PEMemory.INVALID_ADDRESS:
+            addr = self.get_address_with_section(offset, self.runtime_data)
+            if addr != PEMemory.INVALID_ADDRESS:
+                decorate_name = self.get_string(addr, self.runtime_data)
+                if decorate_name != PEMemory.INVALID_ADDRESS:
+                    offset += len(decorate_name)
+                    vtable = self.get_vtable_by_name(decorate_name, decorated=True)
+                    if vtable != PEMemory.INVALID_ADDRESS:
+                        type_name = self.undecorate_symbol_name(decorate_name)
+                        self.type_descriptor_names[type_name] = vtable
+        self.type_descriptor_names = dict(sorted(self.type_descriptor_names.items(), key=lambda item: item[0]))
+
+class RTTIHelper:
+    class RTTIBaseClassDescriptor:
+        _BCD_NOTVISIBLE = 0x00000001
+        _BCD_AMBIGUOUS = 0x00000002
+        _BCD_PRIVORPROTINCOMPOBJ = 0x00000004
+        _BCD_PRIVORPROTBASE = 0x00000008
+        _BCD_VBOFCONTOBJ = 0x00000010
+        _BCD_NONPOLYMORPHIC = 0x00000020
+
+        def __init__(self, descriptor_ptr: int, mem: PEMemory):
+            # reference to type description
+            self.pTypeDescriptor = mem.get_int(descriptor_ptr)
+
+            # #of sub elements within base class array
+            self.num_contained_bases = mem.get_int(descriptor_ptr + 4)
+
+            # member displacement
+            self.mdisp = mem.get_int(descriptor_ptr + 8)
+
+            # vftable displacement
+            self.vdisp = mem.get_int(descriptor_ptr + 12)
+
+            # displacement within vftable
+            self.dispwf = mem.get_int(descriptor_ptr + 16)
+
+            # base class attributes
+            self.attributes = mem.get_int(descriptor_ptr + 20)
+
+            # reference to class hierarchy descriptor
+            self.pClassDescriptor = mem.get_int(descriptor_ptr + 24)
+
+        def is_not_visible(self) -> bool:
+            return bool(self.attributes & self._BCD_NOTVISIBLE)
+
+        def is_unknown_inherit(self) -> bool:
+            return bool(self.attributes & self._BCD_AMBIGUOUS)
+
+        def is_private_or_protected(self) -> bool:
+            return bool(self.attributes & self._BCD_PRIVORPROTINCOMPOBJ)
+
+        def is_base_private_or_protected(self) -> bool:
+            return bool(self.attributes & self._BCD_PRIVORPROTBASE)
+
+        def is_pure_virtual(self) -> bool:
+            return bool(self.attributes & self._BCD_VBOFCONTOBJ)
+
+        def is_non_polymorphic(self) -> bool:
+            return bool(self.attributes & self._BCD_NONPOLYMORPHIC)
+
+    class RTTIClassHierarchyDescriptor:
+        _CHD_MULTINH = 0x00000001
+        _CHD_VIRTINH = 0x00000002
+        _CHD_AMBIGUOUS = 0x00000004
+
+        def __init__(self, descriptor_ptr: int, mem: PEMemory):
+            # the value always is 0
+            self.signature = 0
+
+            # attributes
+            self.attributes = mem.get_int(descriptor_ptr + 4)
+
+            # #of items in the array of base classes
+            self.num_base_classes = mem.get_int(descriptor_ptr + 8)
+
+            # reference to the array of base classes
+            self.reference_array_of_base_classes = mem.get_int(descriptor_ptr + 12)
+            self.array_of_base_classes: list[RTTIHelper.RTTIBaseClassDescriptor] = []
+
+            if self.num_base_classes > 1:
+                for i in range(self.num_base_classes):
+                    if i == 0:
+                        continue # pass self
+                    addr_base_class_descriptor = mem.get_int(self.reference_array_of_base_classes + i * 4)
+                    base_class_descriptor = RTTIHelper.RTTIBaseClassDescriptor(addr_base_class_descriptor, mem)
+                    self.array_of_base_classes.append(base_class_descriptor)
+
+
+        def is_multiple_inherit(self):
+            return bool(self.attributes & self._CHD_MULTINH)
+
+        def is_virtual_inherit(self):
+            return bool(self.attributes & self._CHD_VIRTINH)
+
+        def is_unknown_inherit(self):
+            return bool(self.attributes & self._CHD_AMBIGUOUS)
+
+    class RTTICompleteObjectLocator:
+        def __init__(self, locator_ptr: int, mem: PEMemory):
+            # signature
+            self.signature = mem.get_int(locator_ptr)
+
+            # offset of this vtable in complete class (from top)
+            self.offset = mem.get_int(locator_ptr + 4)
+
+            # offset of constructor displacement
+            self.cdOffset = mem.get_int(locator_ptr + 8)
+
+            # reference to type description
+            self.pTypeDescriptor = mem.get_int(locator_ptr + 12)
+
+            # reference to hierarchy description
+            self.hierarchyDescriptor = RTTIHelper.RTTIClassHierarchyDescriptor(mem.get_int(locator_ptr + 16), mem)
+
+            # reference to object's base
+            self.pSelf = locator_ptr
+
+    def __init__(self, mem: PEMemory):
+        self.mem = mem
+
+    def get_object_locator(self, vtable_ptr: int):
+        return self.RTTICompleteObjectLocator(self.mem.get_long(vtable_ptr - 8)  - self.mem.pe.OPTIONAL_HEADER.ImageBase, self.mem)
 
 class SigMaker:
     def __init__(self, mem: PEMemory):
