353f9d8eaa9434636372a1da7ae5c1bfaddbb9e5
Ciallo <dison2233@126.com>
2024-10-15 18:07:44 +0800

    Update class_dumper.py

--- class_dumper.py
+++ class_dumper.py
@@ -90,7 +90,7 @@ if __name__ == '__main__':
     _game_path = "E:/Steam/steamapps/common/Counter-Strike Global Offensive/game/"
     dumper = ClassDumper(_game_path, filter_type_name)
     # "server", "engine2", "matchmaking", "tier0", "networksystem"
-    libs = ["engine2"]
+    libs = ["server", "engine2", "matchmaking", "tier0", "networksystem"]
     threads = []
     with ThreadPoolExecutor(max_workers=len(libs)) as executor:
         executor.map(dumper.load_library, libs)
